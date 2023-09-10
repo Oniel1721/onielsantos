2f53f5ad13cc7b32ac17fb79c7456c0f61ac563f
Oniel1721 <oni1san7alv21@gmail.com>
2023-09-10 10:46:40 -0400

    chore: metatags optimized and icons moved

---
+++ src/data/skills.ts
@@ -0,0 +1,54 @@
+export const skills = [
+  {
+    icon: "/icons/js-icon.svg",
+    name: "JavaScript",
+  },
+  {
+    icon: "/icons/aws-icon.svg",
+    name: "Amazon Web Services",
+  },
+  {
+    icon: "/icons/postgresql-icon.svg",
+    name: "PostgreSQL",
+  },
+  {
+    icon: "/icons/nestjs-icon.svg",
+    name: "NestJS",
+  },
+  {
+    icon: "/icons/html-icon.svg",
+    name: "HTML",
+  },
+  {
+    icon: "/icons/css-icon.svg",
+    name: "CSS",
+  },
+  {
+    icon: "/icons/docker-icon.svg",
+    name: "Docker",
+  },
+  {
+    icon: "/icons/nodejs-icon.svg",
+    name: "NodeJS",
+  },
+  {
+    icon: "/icons/redux-icon.svg",
+    name: "Redux",
+  },
+  {
+    icon: "/icons/react-icon.svg",
+    name: "React",
+  },
+  {
+    icon: "/icons/graphql-icon.svg",
+    name: "GraphQL",
+  },
+  {
+    icon: "/icons/typescript-icon.svg",
+    name: "TypeScript",
+  },
+  {
+    icon: "/icons/nextjs-icon.svg",
+    name: "NextJS",
+  },
+];
